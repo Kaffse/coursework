4bd8b798cbf96ebf4b79cc07ca5099d0cc46c7ed
Kaffse <kaffse@gmail.com>
2014-03-10 23:36:35 +0000

    Working on init function

--- StartingPoint/diskdriver.c
+++ StartingPoint/diskdriver.c
@@ -1,7 +1,25 @@
 #include "diskdriver.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthreads.h>
 
 void init_disk_driver(DiskDevice dd, void *mem_start, unsigned long mem_length, FreeSectorDescriptorStore *fsds_ptr)
 {
+    //diskdevice
+    DiskDevice *disk;
+    disk = dd;
+
+    //manipluate memory for SectorDiscriptorStore
+    *fsds_prt = create_fsds();
+    create_free_sector_discriptors(fsds_ptr, mem_start, mem_lenght);
+
+    //allocate buffers
+    BoundedBuffer readbuf = createBB(100);
+    BoundedBuffer writebuf = createBB(100);
+    BoundedBuffer responsebuf = createBB(200);
+
+    //sort out threading
+
 }
 
 void blocking_write_sector(SectorDescriptor sd, Voucher *v)
