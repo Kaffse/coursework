e10f3b1f9482769d0763da26ffc192789c5e7a77
Keir Smith <kaffse@gmail.com>
2014-10-14 14:23:59 +0100

    Got package grabber working

---
+++ scripts/package_grabber.py
@@ -0,0 +1,15 @@
+import requests as r
+import string
+from BeautifulSoup import BeautifulSoup as soup
+
+#url = 'http://dl.fedoraproject.org/pub/fedora/linux/releases/20/'
+url = 'http://dl.fedoraproject.org/pub/fedora/linux/releases/20/Everything/x86_64/os/Packages/'
+
+for c in string.ascii_lowercase:
+    html = soup(r.get(url + c).text)
+    for tag in html.findAll('a', {'href': True}):
+        dir_name = tag.attrMap['href']
+        file = r.get(url + c + '/' + dir_name)
+        if 'content-length' in file.headers:
+            size = file.headers['content-length']
+            print dir_name + " " + size
