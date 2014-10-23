64811cf4283675af6c2abd454e78b7187c846d6b
Keir Smith <kaffse@gmail.com>
2014-10-23 12:42:55 +0100

    Added Fib.hs

---
+++ coursework/Fib.hs
@@ -0,0 +1,10 @@
+------------------------------------------------------------------------
+-- Fibonacci number generator
+------------------------------------------------------------------------
+
+module Fib where
+
+import HDL.Hydra.Core.Lib
+import HDL.Hydra.Circuits.StandardCircuits
+
+
