c677dfb78287843279e750362b2f54bacde91d21
Keir Smith <kaffo@higgs.nat.imlach.co.uk>
2014-11-17 01:09:16 +0000

    Some structure

--- plugin/recommend.py
+++ plugin/recommend.py
@@ -10,19 +10,21 @@ class Recommend(dnf.Plugin):
     def __init__(self, base, cli):
         self.base = base
         self.cli = cli
-        if cli is None:
-            self._out('loaded.')
-        else:
-            self._out('loaded (with CLI)')
+        if self.cli is not None:
+            self.cli.register_command(RecommendCommand)
 
-    def _out(self, msg):
-        logger.debug('Recommend plugin: %s', msg)
+#    def _out(self, msg):
+#        logger.debug('Recommend plugin: %s', msg)
+#
+#    def config(self):
+#        self._out('config')
+#
+#    def sack(self):
+#        self._out('sack')
+#
+#    def transaction(self):
+#        self._out('transaction')
 
-    def config(self):
-        self._out('config')
-
-    def sack(self):
-        self._out('sack')
-
-    def transaction(self):
-        self._out('transaction')
+class RecommendCommand(dnf.cli.Command):
+    aliases = ['recommend']
+    summary = _('Makes a recommendation based on your currently installed packaged')
