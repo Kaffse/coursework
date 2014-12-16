ce87b3468ffb1ed7f8adfe245eeffe0a5010b3ef
Keir Smith <kaffo@higgs.nat.imlach.co.uk>
2014-12-16 02:19:15 +0000

    Progress update, some minor changes

--- plugin/recommend.py
+++ plugin/recommend.py
@@ -18,6 +18,17 @@ def uninstalled_package(package, uid):
     data = json.dumps({'package':package, 'uid':uid})
     return r.post(SERVER_ADDRESS + '/uninstall', data)
 
+def print_recommend(package_list):
+    ui_type = '1'
+    if ui_type == '1':
+        print "Prompt Install"
+    elif ui_type == '2':
+        print "Auto Install"
+    elif ui_type == '3':
+        print "List Choice"
+    else:
+        print "UI Selection Error"
+
 class Recommend(dnf.Plugin):
 
     name = 'recommend'
@@ -25,20 +36,28 @@ class Recommend(dnf.Plugin):
     def __init__(self, base, cli):
         self.base = base
         self.cli = cli
+
         if self.cli is not None:
             self.cli.register_command(RecommendCommand)
 
-#    def _out(self, msg):
-#        logger.debug('Recommend plugin: %s', msg)
-#
-#    def config(self):
-#        self._out('config')
-#
-#    def sack(self):
-#        self._out('sack')
-#
-#    def transaction(self):
-#        self._out('transaction')
+    def get_installed(self):
+        query = self.base.sack.query()
+        installed = query.installed()
+        return list(installed)
+
+    def _out(self, msg):
+        logger.debug('Recommend plugin: %s', msg)
+
+    #this order holds true when updating/installing packages
+    def config(self):
+        self._out('config')
+
+    def sack(self):
+        self._out('sack')
+        self._out(self.get_installed())
+
+    def transaction(self):
+        self._out('transaction')
 
 class RecommendCommand(dnf.cli.Command):
     aliases = ['recommend']
@@ -46,15 +65,38 @@ class RecommendCommand(dnf.cli.Command):
 
     def update_packages(self, packagelist, uid):
         data = json.dumps({'packagelist':packagelist, 'uid':uid})
-        response =  r.post(SERVER_ADDRESS + '/update', data)
+        response = r.post(SERVER_ADDRESS + '/update', data)
+        print SERVER_ADDRESS + '/update'
         return response.text
 
-    def get_recommend_list(uid):
+    def get_recommend_list(self, uid):
         data = json.dumps({'uid':uid})
         return r.post(SERVER_ADDRESS + '/recommend', data)
 
     def run(self, extcmds):
         """Execute the command."""
 
+        #placeholder for recommend list
         list = [100]
-        print(self.update_packages(list ,100))
+
+        if len(extcmds) > 1:
+            print "Invalid Argments for Recommend plugin"
+        elif len(extcmds) == 0:
+            print_recommend(self.get_recommend_list(100))
+        elif extcmds[0].lower() == "update":
+            print_recommend(self.update_packages(list, 100))
+        else:
+            print "Invalid Argments for Recommend plugin"
+
+# implement A/B UI
+# auto install - prompt based - list
+
+
+# write working config
+# num of reccomends, ip of server
+
+
+# association rule mining??
+# Look into ways to store backend table stuff
+# Finish up shiney frontend
+# write survey questions on A/B UI
--- server/app.py
+++ server/app.py
@@ -1,30 +1,41 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route('/register')
 def register():
     if request.method == 'POST':
-        return "PlaceHolder"
+        print request.get_json()
+        data = {'success':'true'}
+        return json.dumps(data)
 
 @app.route('/install')
 def install():
     if request.method == 'POST':
-        return "PlaceHolder"
+        print request.get_json()
+        data = {'list':[]}
+        return json.dumps(data)
 
 @app.route('/uninstall')
 def uninstall():
     if request.method == 'POST':
-        return "PlaceHolder"
+        print request.get_json()
+        data = {'list':[]}
+        return json.dumps(data)
 
-@app.route('/update')
+@app.route('/update', methods = ['GET', 'POST'])
 def update():
     if request.method == 'POST':
-        return "PlaceHolder"
+        print request.get_json()
+        data = {'list':[]}
+        return json.dumps(data)
 
 @app.route('/recommend')
 def recommend():
     if request.method == 'POST':
-        return "PlaceHolder"
+        print request.get_json()
+        data = {'list':[]}
+        return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(port='80')
