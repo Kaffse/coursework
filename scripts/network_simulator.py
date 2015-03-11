c9931248e7be4dced7ca658f508502dfe4da48d5
Kaffse <kaffse@gmail.com>
2015-03-11 10:55:47 +0000

    Added node class, ready for routing algo

--- scripts/network_simulator.py
+++ scripts/network_simulator.py
@@ -1,5 +1,28 @@
 import sys
 
+class Node:
+    def __init__(self, node_num, cost_list):
+        self.id = node_num
+        self.cost_map = {}
+
+        for i in range(len(cost_list)):
+            if cost_list[i] != 'x':
+                self.cost_map[i] = (i, int(cost_list[i]))
+
+    def __str__(self):
+        return str(self.id)
+
+    def update(self, dest, recived_from, distance):
+        if dest not in self.cost_map.keys():
+            self.cost_map[dest] = (recieved_from, distance)
+        elif self.cost_map[dest] > distance:
+            self.cost_map[dest] = (recieved_from, distance)
+
+    def pretty_print(self):
+        print str(self.id)
+        for node in self.cost_map.keys():
+            print str(node) + ":" + str(self.cost_map[node])
+
 filename = raw_input("Filename of network file: ")
 
 try:
@@ -8,17 +31,22 @@ except:
     print "Error opening file"
     sys.exit(0)
 
-cost_matrix = []
+node_matrix = []
+i = 0
 
 for line in file:
     #remove the newline
     line = line[:-1]
 
     #pack information into a datastruture
-    cost_matrix += [line.split()]
+    node_matrix += [Node(i, line.split())]
+    i += 1
 
 #set n to be total number of nodes in the network
-n = len(cost_matrix)
+n = len(node_matrix)
 
-for node in range(n):
+for node in node_matrix:
+    node.pretty_print()
+
+#for node in range(n):
     #routing stuff
