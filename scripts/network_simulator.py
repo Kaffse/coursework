2da3c51ada32ce761999cdad19633d08d9cbda5a
Kaffse <kaffse@gmail.com>
2015-03-13 12:54:21 +0000

    Implementation nearly done, need to fix split horizon

--- scripts/network_simulator.py
+++ scripts/network_simulator.py
@@ -6,6 +6,8 @@ class Node:
         self.id = node_num
         self.cost_map = {}
         self.adj_nodes = []
+        self.failure_settings = {}
+        self.step_count = 0
 
         self.cost_map[self.id] = (self.id, 0)
         for i in range(len(cost_list)):
@@ -37,6 +39,13 @@ class Node:
         self.adj_nodes = adj_list
 
     def step_routing(self, split_horizon):
+        self.step_count += 1
+        if self.step_count in self.failure_settings.keys():
+            this_failure = self.failure_settings[self.step_count]
+            if this_failure[1] < 0:
+                del(self.cost_map[this_failure[0]])
+            else:
+                self.cost_map[this_failure[0]][1] = this_failure[1]
         for node in self.adj_nodes:
             temp_map = self.cost_map
             if split_horizon:
@@ -44,6 +53,16 @@ class Node:
                     if temp_map[dest][0] == node:
                         del(temp_map[dest])
             node.update(temp_map, self.id)
+
+    def set_failure(self, step_num, dest_link, new_cost):
+        self.failure_settings[step_num] = (dest_link, new_cost)
+        if new_cost < 0:
+            for node in self.adj_nodes:
+                if node.id == dest_link:
+                    self.adj_nodes.remove(node)
+            print "Set link " + str(self.id) + " -> " + str(dest_link) + " to fail on step " + str(step_num)
+        else:
+            print "Set link " + str(self.id) + " -> " + str(dest_link) + " to " + str(new_cost) + "  on step " + str(step_num)
 
     def pretty_print(self):
         print str(self.id)
@@ -72,6 +91,9 @@ def load_file():
 
     return node_matrix
 
+print "ANC4 Network Simulator"
+print "Commands are as follows: load, tables, preset, route, trace, split, help, quit"
+print "Please use help for speicifc help on each command"
 com = string.lower(raw_input(">"))
 
 node_matrix = []
@@ -94,8 +116,15 @@ while com != "quit":
                 node.step_routing(split_horizon)
         for node in node_matrix:
             node.pretty_print()
+
     elif com == "preset":
-        print "preset"
+        cycle = int(raw_input("Fail on cycle #: "))
+        source = int(raw_input("Origin node of change: "))
+        dest = int(raw_input("Destination node of change: "))
+        change = int(raw_input("Change of cost: "))
+        node_matrix[source].set_failure(cycle, dest, change)
+        node_matrix[dest].set_failure(cycle, source, change)
+
     elif com == "route":
         source = int(raw_input("Source Node: "))
         dest = int(raw_input("Destination Node: "))
@@ -111,6 +140,7 @@ while com != "quit":
                 cur_node = node_matrix[cur_node.cost_map[dest][0]]
         else:
             print "No route avalaible"
+
     elif com == "trace":
         nodes = raw_input("Nodes to trace: ")
         cycles = int(raw_input("Number of cycles: "))
@@ -129,4 +159,19 @@ while com != "quit":
         else:
             print "Enabling Split Horizon"
             split_horizon = True
+
+    elif com == "help":
+        com_help = raw_input("Which command do you need help with: ")
+        if com_help == "load":
+            print "Will prompt you for a valid network file, this is required to use the other commands. Use this to reset routing tables"
+        elif com_help == "tables":
+            print "Prompted for # of cycles to preform before printing out all the routing tables for all nodes. Use 0 to print current tables"
+        elif com_help == "preset":
+            print "Promted for the cycle the change will occur, one node in the link, the other node in the link and the cost change. A cost chance to -1 indicates failure"
+        elif com_help == "route":
+            print "Promted for the soruce and destination nodes of the route and how many cycle of the routing algoritm to preform. Use 0 to print current route"
+        elif com_help == "trace":
+            print "Prompted for the nodes of interest, seperated by a single space, then the number of cycles of the routing algoritm to preform"
+        elif com_help == "split":
+            print "Switches split horizon on and off in all routing algoritms. Defaulted to off"
     com = string.lower(raw_input(">"))
